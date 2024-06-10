3620785b75952c34ee3ae39cb92202dd9b318d9c
Moses1509 <cococran123@gmail.com>
2024-06-10 12:53:29 +0700

    ssnake

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Moses Aripin Indah Setiawan
+                Saya Tinggal diBogor Dramaga
+                Hobi Saya adalah Bermain Game";
+    }
+}
