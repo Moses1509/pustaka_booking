93d0fd932602ba58243b6e2d162371a2c7a3e7fb
Moses1509 <cococran123@gmail.com>
2024-06-17 20:53:00 +0700

    Pertemuan 12

--- application/views/autentifikasi/registrasi.php
+++ application/views/autentifikasi/registrasi.php
@@ -1,7 +1,6 @@
 <div class="container">
 
-    <div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx
-auto">
+    <div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
         <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
@@ -12,11 +11,11 @@ auto">
                         </div>
                         <form class="user" method="post" action="<?= base_url('autentifikasi/registrasi'); ?>">
                             <div class="form-group">
-                                <input type="text" class="formcontrol form-control-user" id="nama" name="nama" placeholder="Nama Lengkap" value="<?= set_value('nama'); ?>">
+                                <input type="text" class="form-control form-control-user" id="nama" name="nama" placeholder="Nama Lengkap" value="<?= set_value('nama'); ?>">
                                 <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="formcontrol form-control-user" id="email" name="email" placeholder="Alamat Email" value="<?= set_value('email'); ?>">
+                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Alamat Email" value="<?= set_value('email'); ?>">
                                 <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group row">
